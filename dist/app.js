ae0ce47ae33807c503eb40730e20f888169a7878
b.konotopskii <b.konotopskii@plarium.com>
2017-06-17 11:49:42 +0300

    + colony base logic

--- dist/app.js
+++ dist/app.js
@@ -135,11 +135,45 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+const INITIAL_FORCE = 10;
+const ATTACK_MODIFICATOR = 0.65;
+const MIN_ATTACK_REQUIREMENT = 25;
+
 class Colony extends Phaser.Sprite {
   constructor(game, x, y, imageName) {
     super(game, x, y, imageName);
+
+    this.force = INITIAL_FORCE;
   }
 
+  // @TODO: consume from colony force 60%
+  // do not allow to attack if colony force is not enough (25 poins min)
+  attack() {
+    if (this.canAttack()) {
+      const attackPower = MATH.round(this.force * ATTACK_MODIFICATOR);
+
+      this.changeForce(attackPower);
+    }
+  }
+
+  canAttack() {
+    return this.force >= MIN_ATTACK_REQUIREMENT;
+  }
+
+  // @TODO: add num to current colony force
+  reinforce(num) {
+    this.changeForce(num);
+  }
+
+  changeForce(num) {
+    if (Number.isInteger(num)) {
+      this.force += num;
+    } else {
+      console.warn(`reinforce: [${num}] should be an integer`);
+    }
+
+    return this.force;
+  }
 }
 /* harmony export (immutable) */ __webpack_exports__["a"] = Colony;
 
--- src/classes/colony.js
+++ src/classes/colony.js
@@ -1,7 +1,40 @@
+const INITIAL_FORCE = 10;
+const ATTACK_MODIFICATOR = 0.65;
+const MIN_ATTACK_REQUIREMENT = 25;
+
 export default class Colony extends Phaser.Sprite {
   constructor(game, x, y, imageName) {
     super(game, x, y, imageName);
+
+    this.force = INITIAL_FORCE;
   }
 
+  // @TODO: consume from colony force 60%
+  // do not allow to attack if colony force is not enough (25 poins min)
+  attack() {
+    if (this.canAttack()) {
+      const attackPower = MATH.round(this.force * ATTACK_MODIFICATOR);
 
+      this.changeForce(attackPower);
+    }
+  }
+
+  canAttack() {
+    return this.force >= MIN_ATTACK_REQUIREMENT;
+  }
+
+  // @TODO: add num to current colony force
+  reinforce(num) {
+    this.changeForce(num);
+  }
+
+  changeForce(num) {
+    if (Number.isInteger(num)) {
+      this.force += num;
+    } else {
+      console.warn(`reinforce: [${num}] should be an integer`);
+    }
+
+    return this.force;
+  }
 }
