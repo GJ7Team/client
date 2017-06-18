e334d1a4151beca2ea923d6ec6506228fdd69e37
Sappho <murderedsappho@mail.ru>
2017-06-18 14:56:25 +0300

    Buttons

--- src/global/menuState.js
+++ src/global/menuState.js
@@ -21,13 +21,9 @@ function goToScoreboardState() {
 export default {
   preload: function() {
     const game = this.game;
-    this.back = game.load.spritesheet(
-      'button',
-      'assets/buttons/button_sprite_sheet.png',
-      193,
-      71
-    );
     this.game.load.image('background', 'assets/images/background.png');
+    this.game.load.image('play', 'assets/buttons/play.png');
+    this.game.load.image('scoreboard', 'assets/buttons/scoreboard.png');
   },
 
   create: function() {
@@ -41,30 +37,22 @@ export default {
     const button1 = game.add.button(
       game.world.centerX,
       game.world.centerY * 0.5,
-      'button',
+      'play',
       goToSeachMatch.bind(this),
-      this,
-      2,
-      1,
-      0
+      this
     );
     button1.name = 'Play';
     button1.anchor.setTo(0.5, 0.5);
-    button1.scale.setTo(1.5, 1.5);
 
     //  SCOREBOARD BUTTON
     const button2 = game.add.button(
       game.world.centerX,
       game.world.centerY * 1.05,
-      'button',
+      'scoreboard',
       goToScoreboardState.bind(this),
-      this,
-      2,
-      1,
-      0
+      this
     );
     button2.name = 'Scoreboard';
     button2.anchor.setTo(0.5, 0.5);
-    button2.scale.setTo(1, 1);
   },
 };
