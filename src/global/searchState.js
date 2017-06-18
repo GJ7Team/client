c04fab413a1ebbde316869ea885aa7516c376039
Sappho <murderedsappho@mail.ru>
2017-06-18 16:20:49 +0300

    Back for search

--- src/global/searchState.js
+++ src/global/searchState.js
@@ -6,6 +6,12 @@ import initScaling from 'util/initScaling';
 export default {
   preload: function() {
     this.game.load.image('background', 'assets/images/background.png');
+    this.button = this.game.load.spritesheet(
+      'back',
+      'assets/buttons/back.png',
+      193,
+      71
+    );
 
     actions.matchFind();
 
@@ -25,7 +31,18 @@ export default {
     this.game.stage.disableVisibilityChange = true;
     initScaling(this.game).create();
     const background = this.game.add.sprite(0, 0, 'background');
+    const backButton = this.game.add.button(
+      10,
+      10,
+      'back',
+      () => this.game.state.start(STATES.MENU),
+      this,
+      2,
+      1,
+      0
+    );
 
+    backButton.scale.setTo(0.3, 0.3);
     addGradientText(this.game, {
       text: 'Searching a match...',
     });
